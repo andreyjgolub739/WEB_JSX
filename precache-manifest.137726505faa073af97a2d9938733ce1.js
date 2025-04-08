f3952b12cdaeb7de708b08ce5a38ad8a56ec2d3d
andreyjgolub739 <andreyjgolub739@gmail.com>
2025-04-08 21:22:43 +0300

    Updates

---
+++ precache-manifest.137726505faa073af97a2d9938733ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94aec095d5cb93bab9018641975882b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2fc29a818c933e402f4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "352051d2182d97ae5d44",
+    "url": "/static/js/2.78f65707.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.78f65707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d77812b17bca1930732",
+    "url": "/static/js/3.85523f28.chunk.js"
+  },
+  {
+    "revision": "b2fc29a818c933e402f4",
+    "url": "/static/js/main.7137baad.chunk.js"
+  },
+  {
+    "revision": "193bfeee98b8ff5f5fc7",
+    "url": "/static/js/runtime-main.a5f6f956.js"
+  }
+]);
