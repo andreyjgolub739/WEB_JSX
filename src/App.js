c14b1554015c93c0ced912fd46dc6e786a0ab93e
andreyjgolub739 <andreyjgolub739@gmail.com>
2025-04-08 20:36:34 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+// Завдання 1: Відображення динамчного контенту
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserCard name="Andrey" age="32" />
+      <StyledText />
+      <ShowList />
     </div>
   );
+}
+
+function UserCard(props) {
+  return <p>
+          Ім'я користувача: {props.name},
+          <br/>
+          {props.age}
+         </p>;
+}
+
+// Завдання 2: Додавання стилів
+function StyledText() {
+  const styles = {
+    color: 'blue',
+    fontsize: '20px'
+  };
+
+  return <p style={styles}>COLOR!</p>;
+}
+
+// Завдання 3: Відображення списку
+function ShowList() {
+  const items = ['1', '2', '3'];
+
+  const list = (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+
+  return list;
 }
 
 export default App;
